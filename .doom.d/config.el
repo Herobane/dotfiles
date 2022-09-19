aed164608f367e7092b1538edc177c5250ea77fb
Flavien A <flav.anc@pm.me>
2022-09-19 19:06:42 +0200

    Updated my doom emacs config

--- .doom.d/config.el
+++ .doom.d/config.el
@@ -78,28 +78,6 @@
 ;; CUSTOM
 (setq org-hide-emphasis-markers t)
 
-;;(let* ((variable-tuple
-;;    (cond ((x-list-fonts "ETBembo")         '(:font "ETBembo"))
-;;        ((x-list-fonts "Source Sans Pro") '(:font "Source Sans Pro"))
-;;        ((x-list-fonts "Lucida Grande")   '(:font "Lucida Grande"))
-;;        ((x-list-fonts "Verdana")         '(:font "Verdana"))
-;;        ((x-family-fonts "Sans Serif")    '(:family "Sans Serif"))
-;;        (nil (warn "Cannot find a Sans Serif Font.  Install Source Sans Pro."))))
-;;    (base-font-color     (face-foreground 'default nil 'default))
-;;    (headline           `(:inherit default :weight bold :foreground ,nil)))
-;;
-;;(custom-theme-set-faces
-;;'user
-;;`(org-level-8 ((t (,@headline ,@variable-tuple))))
-;;`(org-level-7 ((t (,@headline ,@variable-tuple))))
-;;`(org-level-6 ((t (,@headline ,@variable-tuple))))
-;;`(org-level-5 ((t (,@headline ,@variable-tuple))))
-;;`(org-level-4 ((t (,@headline ,@variable-tuple :height 1.1))))
-;;`(org-level-3 ((t (,@headline ,@variable-tuple :height 1.25))))
-;;`(org-level-2 ((t (,@headline ,@variable-tuple :height 1.5))))
-;;`(org-level-1 ((t (,@headline ,@variable-tuple :height 1.75))))
-;;`(org-document-title ((t (,@headline ,@variable-tuple :height 2.0 :underline nil))))))
-
 (custom-set-faces
  '(org-level-1 ((t (:inherit outline-1 :height 1.5))))
  '(org-level-2 ((t (:inherit outline-2 :height 1.4))))
--- .doom.d/init.el
+++ .doom.d/init.el
@@ -115,7 +115,7 @@
        :lang
        ;;agda              ; types of types of types of types...
        ;;beancount         ; mind the GAAP
-       ;;(cc +lsp)         ; C > C++ == 1
+       (cc +lsp)         ; C > C++ == 1
        ;;clojure           ; java with a lisp
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
@@ -145,16 +145,16 @@
        javascript        ; all(hope(abandon(ye(who(enter(here))))))
        ;;julia             ; a better, faster MATLAB
        ;;kotlin            ; a better, slicker Java(Script)
-       ;;latex             ; writing papers in Emacs has never been so fun
+       latex             ; writing papers in Emacs has never been so fun
        ;;lean              ; for folks with too much to prove
        ;;ledger            ; be audit you can be
-       ;;lua               ; one-based indices? one-based indices
+       lua               ; one-based indices? one-based indices
        markdown          ; writing docs for people to ignore
        ;;nim               ; python + lisp at the speed of c
-       ;;nix               ; I hereby declare "nix geht mehr!"
+       nix               ; I hereby declare "nix geht mehr!"
        ;;ocaml             ; an objective camel
        (org +pretty)       ; organize your plain life in plain text
-       ;;php               ; perl's insecure younger brother
+       php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
        python            ; beautiful is better than ugly
